80118003c23fbcf8148c7877c43acf9f29f414f5
Aaron Baker <bakeraj4@miamioh.edu>
2014-11-22 20:49:31 -0500

    There are now colored particles!

--- SRC/bakeraj4_project.cpp
+++ SRC/bakeraj4_project.cpp
@@ -7,12 +7,16 @@
 #include <time.h>
 #include <string> 
 #include <fstream>
+#include <unordered_map>
 #include <GL/freeglut.h>
 #pragma once
+
 typedef FlockItem Flock;
+typedef std::unordered_map<unsigned int, std::vector<float>> Colors;
+
 const int W = 512, H = 512, SLICES = 25, STACKS = 20;
 std::vector<Flock> allParticles;
-float n, l, m;
+Colors c;
 
 void sphere() {
 	glutSolidSphere(0.01, SLICES, STACKS);
@@ -28,6 +32,10 @@ void IT(double sx, double sy, double sz, double dx, double dy, double dz,
 	glPopMatrix();
 }
 
+void setColor(unsigned int num) {
+	glColor3f((GLfloat)c[num][0], (GLfloat)c[num][1], (GLfloat)c[num][2]);
+	std::cout << "COLOR:\n" << c[num][0] << "\t" << c[num][1] << "\t" << c[num][2] << "\n";
+}
 
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -39,10 +47,7 @@ void display(void) {
 	glLineWidth(4);
 	std::cout << allParticles.size();
 	for (unsigned int i = 0; i < allParticles.size(); i++) {
-		l = (float) i; // the level of the current flock
-		m = (n - l) / n;
-		// set color
-		glColor3f(m, m, m);
+		setColor(i);
 		std::cout << "\nlevel : " << i << "\n";
 		for(unsigned int j = 0; j < allParticles[i].getAmnt(); j++) {
 			std::cout << allParticles[i].getPosX(j) << "\t" << allParticles[i].getPosY(j) << "\t" << allParticles[i].getRotZ(j) << "\n";
@@ -75,6 +80,71 @@ std::vector<Flock> makeAllParticles(std::string& fileName) {
 	return particles;
 }
 
+void setUpColors() {
+	// only 10 colors for now
+	std::vector<float> color;
+	// red
+	color.push_back(1.0f);
+	color.push_back(0.0);
+	color.push_back(0.0);
+	c[0] = color;
+	color.clear();
+	// green
+	color.push_back(0.0f);
+	color.push_back(1.0);
+	color.push_back(0.0);
+	c[1] = color;
+	color.clear();
+	// blue
+	color.push_back(0.0f);
+	color.push_back(0.0);
+	color.push_back(1.0);
+	c[2] = color;
+	color.clear();
+	// white
+	color.push_back(1.0f);
+	color.push_back(1.0);
+	color.push_back(1.0);
+	c[3] = color;
+	color.clear();
+	// orange
+	color.push_back(1.0f);
+	color.push_back(106.0f / 255.0f);
+	color.push_back(0.0);
+	c[4] = color;
+	color.clear();
+	// yellow
+	color.push_back(251.0f / 255.0f);
+	color.push_back(1.0f);
+	color.push_back(56.0f / 255.0f);
+	c[5] = color;
+	color.clear();
+	// purple
+	color.push_back(178.0f / 255.0f);
+	color.push_back(0.0);
+	color.push_back(1.0);
+	c[6] = color;
+	color.clear();
+	// pink
+	color.push_back(1.0f);
+	color.push_back(127.0f / 255.0f);
+	color.push_back(179.0f / 255.0f);
+	c[7] = color;
+	color.clear();
+	// dark pink
+	color.push_back(122.0f / 255.0f);
+	color.push_back(61.0f / 255.0f);
+	color.push_back(86.0f / 255.0f);
+	c[8] = color;
+	color.clear();
+	// grey
+	color.push_back(158.0f / 255.0f);
+	color.push_back(158.0f / 255.0f);
+	color.push_back(158.0f / 255.0f);
+	c[9] = color;
+	color.clear();
+}
+
 int main(int argc, char* argv[]) {
     if (argc != 3) {
         std::cout << "There were not engough parameters.\n"
@@ -91,8 +161,7 @@ int main(int argc, char* argv[]) {
 	srand(time(NULL));
 	std::string file(argv[2]);
 	allParticles = makeAllParticles(file);
-	n = (float) allParticles.size();
-
+	setUpColors();
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
